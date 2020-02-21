b16f0c1a0ccb799ea64be3cccc73e6cf8fb75f4b
Diego Gargallo Tarin <diego.gargallo@sngular.com>
2020-02-21 23:48:17 +0100

    Configure Firebase (Cloud Firestore and Analytics) and test it reading dishes

--- app/src/main/java/es/diegogargallotarin/gula/MainActivity.kt
+++ app/src/main/java/es/diegogargallotarin/gula/MainActivity.kt
@@ -2,11 +2,26 @@ package es.diegogargallotarin.gula
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val db = FirebaseFirestore.getInstance()
+
+        db.collection("platos")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    Log.d("TAG", "${document.id} => ${document.data}")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w("TAG", "Error getting documents.", exception)
+            }
     }
 }
